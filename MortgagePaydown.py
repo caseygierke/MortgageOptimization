e4ed3aee27de6bbcbe10ffff6cdd8e061a1f0033
caseygierke <caseygierke@gmail.com>
2018-02-20 08:01:20 -0700

    Changes as of 2018-02-20

--- MortgagePaydown.py
+++ MortgagePaydown.py
@@ -15,7 +15,7 @@
 
 import matplotlib.pyplot as plt
 
-
+git config --global core.editor “‘C:/Program Files/Notepad++/notepad++.exe’ -multiInst -notabbar -nosession -noPlugin”
 # -------------------------------------------
 # INPUTS
 # -------------------------------------------
